84d8b5d0f94fa431fc806dae9607d8b4a270d4c2
zcx <961519171@qq.com>
2023-04-25 17:38:42 +0800

    空项目提交

---
+++ src/main/java/com/zcx/myblogs/MyblogsApplication.java
@@ -0,0 +1,13 @@
+package com.zcx.myblogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zcx/myblogs/MyblogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zcx.myblogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
